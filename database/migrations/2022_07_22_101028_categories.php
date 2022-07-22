a826f60724cd573e247d511030a035b02c241053
TeterevDaniil <teterevdanil@gmail.com>
2022-07-22 16:37:58 +0600

    second

--- database/migrations/2022_07_22_101028_categories.php
+++ database/migrations/2022_07_22_101028_categories.php
@@ -13,10 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
+            $table->string('description')->null();
               
             $table->timestamps();
         });
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('categories');
     }
 };
